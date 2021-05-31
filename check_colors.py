e79bc80a60e9ccc2a2e9f4571542970f85eaaf27
IgaLewandowska <igalewandowska98@wp.pl>
2021-05-31 19:41:05 +0200

    0.2

--- check_colors.py
+++ check_colors.py
@@ -1,2 +1,22 @@
-import Pillow 
-from colors.py import generate_colors
+#import Pillow 
+#from colors.py import generate_colors
+import colorthief
+from colorthief import ColorThief
+import glob
+from pathlib import Path
+
+def get_colors(image):
+	dominant_color = ColorThief(image).get_color(quality=1)
+	return dominant_color
+
+print (get_colors('blockchains/polygon/info/logo.png'))
+
+def get_files():
+	files = list(Path('blockchains').glob('**/*.png'))
+	return files
+
+print (get_files())
+
+
+
+       
