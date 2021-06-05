ebe824e107b439d7fe6408b9e1d9353f1c51cb51
IgaLewandowska <igalewandowska98@wp.pl>
2021-06-05 17:46:42 +0200

    0.3

--- check_colors.py
+++ check_colors.py
@@ -9,14 +9,30 @@ def get_colors(image):
 	dominant_color = ColorThief(image).get_color(quality=1)
 	return dominant_color
 
-print (get_colors('blockchains/polygon/info/logo.png'))
+#print (get_colors('blockchains/polygon/info/logo.png'))
 
 def get_files():
 	files = list(Path('blockchains').glob('**/*.png'))
 	return files
 
-print (get_files())
+#print (get_files())
 
+def main():
+	colors_list = []
+
+	for i in get_files():
+		color = get_colors(i)
+		name = str(i).split('/')[-2]
+		if name == 'info':
+			name = str(i).split('/')[-3]
+		tmp = {name: color}	
+		colors_list.append(tmp)
+	print(colors_list)	
+
+
+main()
+
+		
 
 
        
